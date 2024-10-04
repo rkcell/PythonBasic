17f331c103f821866eeb27ed2481afeb9d029a45
rkcell <rkcell@gmail.com>
2024-10-04 17:02:12 +0400

    Lesson 8 added

---
+++ lesson8.py
@@ -0,0 +1,20 @@
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist2 = ["melon", "other elements"]
+# print(thislist)
+#
+# thislist.extend(thislist2)
+#
+# # thislist.insert(2, "melon")
+#
+# print(thislist)
+#
+# [print(x) for x in thislist]
+#
+# newlist = [x for x in thislist if "a" in x]
+# print(newlist, type(newlist))
